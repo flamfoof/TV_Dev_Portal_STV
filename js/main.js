a497a635bc237aabcb5a9506d73565109d7ee5ba
Felix <felixlam0316@gmail.com>
2022-05-13 10:01:15 -0400

    - Prevent Disk Cache on getting S3 paths

--- js/main.js
+++ js/main.js
@@ -65,6 +65,7 @@ async function getPath(path) {
     var directory = path;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open('GET', directory, false); // false for synchronous request
+    xmlHttp.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
     xmlHttp.send();
     var ret = xmlHttp.responseText;
 
